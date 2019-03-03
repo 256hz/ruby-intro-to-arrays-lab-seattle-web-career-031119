9e8d4a5fb6a0799d8072c06c7cfe328804493d08
Abe D <iaryiou111@gmail.com>
2019-03-03 21:00:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,8 +6,16 @@ def array_with_two_elements
   ['one', 'five']
 end
 
-def first_element(array, index)
-  array[index]
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[-1]
 end
 
 def first_element_with_array_methods(array)
